d1e9edf0729b079edffdbf1c59c3e3e74d863aff
Butcher45 <79919127+Butcher45@users.noreply.github.com>
2021-03-02 09:11:25 -0500

    Initial Commit
    Initial Commit

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    private TextMeshProUGUI scoreText;
+    public int scoreValue;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void UpdateScore()
+    {
+        scoreText.text = "Score: " + scoreValue;
+    }
+
+
+
+    //public void Update()
+    //{
+    //    scoreText.text = "Score:" + scoreValue;
+    //}
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/addforcecontroller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addforcecontroller : MonoBehaviour
+{
+    public float thrust = 1.0f;
+    public Rigidbody2D rb;
+    public float rotationSpeed = 100.0f;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        
+       
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, -Input.GetAxis("Horizontal") * rotationSpeed);
+        
+        
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.up * thrust * Input.GetAxis("Vertical"));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"));
+
+        {
+            //Debug.Log("You hit an Enemy");
+            Destroy(other.gameObject);
+        }
+
+    }
+
+
+}
+
+
+
+
